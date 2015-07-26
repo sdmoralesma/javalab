3093eafcf062f3bf83c99b63fd3f1a27fbf1ab91
sergio morales <sdmoralesma@gmail.com>
2015-07-26 17:38:45 -0500

    removed unneeded vars

--- javalab/src/main/webapp/resources/js/resize-ace-vertically.js
+++ javalab/src/main/webapp/resources/js/resize-ace-vertically.js
@@ -9,12 +9,10 @@ function resizeTextAreasVertically() {
     var extNavHeight = $('#ext-nav').height();
     //  Define height for each element based on %
     var codeEditorHeight = (windowHeight * 75 / 100) - extNavHeight;
-    var testCodeEditorHeight = (windowHeight * 75 / 100) - extNavHeight;
     var consoleHeight = (windowHeight * 25 / 100) - extNavHeight;
 
     // resize elements
     $('#code-editor').height(codeEditorHeight);
-    $('#test-editor').height(testCodeEditorHeight);
     $('#console').height(consoleHeight);
 }
 
