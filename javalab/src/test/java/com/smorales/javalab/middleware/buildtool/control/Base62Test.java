7727f98e9c303559fbb84ececbe7a49988d2099c
Sergio <sdmoralesma@gmail.com>
2015-08-07 23:49:10 -0500

    added base 62 tool

---
+++ javalab/src/test/java/com/smorales/javalab/middleware/buildtool/control/Base62Test.java
@@ -0,0 +1,46 @@
+package com.smorales.javalab.middleware.buildtool.control;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Base62Test {
+
+    @Test
+    public void testCharList() throws Exception {
+        StringBuilder sb = new StringBuilder();
+        for (char c = 'a'; c <= 'z'; c++) {
+            sb.append(c);
+        }
+        for (char c = 'A'; c <= 'Z'; c++) {
+            sb.append(c);
+        }
+        for (int i = 0; i <= 9; i++) {
+            sb.append(i);
+        }
+        assertThat(Base62.ALPHABET).isEqualTo(sb.toString());
+    }
+
+    @Test
+    public void testStringFromInt() throws Exception {
+        int n = 0;
+        String str = "6JaY2";
+        char[] chars = str.toCharArray();
+        n += Base62.ALPHABET.indexOf(chars[0]) * (int) Math.pow(62, 4);
+        n += Base62.ALPHABET.indexOf(chars[1]) * (int) Math.pow(62, 3);
+        n += Base62.ALPHABET.indexOf(chars[2]) * (int) Math.pow(62, 2);
+        n += Base62.ALPHABET.indexOf(chars[3]) * (int) Math.pow(62, 1);
+        n += Base62.ALPHABET.indexOf(chars[4]) * (int) Math.pow(62, 0);
+        assertThat(Base62.fromBase10(n)).isEqualTo(str);
+    }
+
+    @Test
+    public void testIntegerFromString() throws Exception {
+        assertThat(Base62.toBase10("cb")).isEqualTo(125);
+    }
+
+    @Test
+    public void testIntegerWithOffsetFromInt() throws Exception {
+        assertThat(Base62.fromBase10WithOffset(1, 1000000)).isEqualTo("emjd");
+    }
+}
