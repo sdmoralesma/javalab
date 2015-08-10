03d9991dd59a25d7348617877f17b42a844a5ac4
Sergio Morales <Sergio.Morales2@wnco.com>
2015-08-10 11:51:16 -0500

    simplified jaxrs

--- javalab/src/main/java/com/smorales/javalab/middleware/config/JAXRSConfiguration.java
+++ javalab/src/main/java/com/smorales/javalab/middleware/config/JAXRSConfiguration.java
@@ -1,22 +1,9 @@
 package com.smorales.javalab.middleware.config;
 
-import com.smorales.javalab.middleware.workspaceprocessor.boundary.rest.WorkspaceProcessorResource;
-
+import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
-import java.util.Set;
 
-@javax.ws.rs.ApplicationPath("rest")
+@ApplicationPath("rest")
 public class JAXRSConfiguration extends Application {
 
-    @Override
-    public Set<Class<?>> getClasses() {
-        Set<Class<?>> resources = new java.util.HashSet<>();
-        addRestResourceClasses(resources);
-        return resources;
-    }
-
-    private void addRestResourceClasses(Set<Class<?>> resources) {
-        resources.add(WorkspaceProcessorResource.class);
-        resources.add(CORSFilter.class);
-    }
 }
