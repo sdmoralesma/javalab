70be70108177d05f384fcbcd3b3c804dfe85613e
Sergio <sdmoralesma@gmail.com>
2016-06-12 00:18:45 +0200

    added mode

--- javalab/src/main/java/com/smorales/javalab/workspaceprocessor/control/Language.java
+++ javalab/src/main/java/com/smorales/javalab/workspaceprocessor/control/Language.java
@@ -1,16 +1,18 @@
 package com.smorales.javalab.workspaceprocessor.control;
 
 public enum Language {
-    GROOVY("groovy", ".groovy"),
-    JAVA("java", ".java"),
-    SCALA("scala", ".scala");
+    GROOVY("groovy", ".groovy", "text/x-groovy"),
+    JAVA("java", ".java", "text/x-java"),
+    SCALA("scala", ".scala", "text/x-scala");
 
     private String description;
     private String extension;
+    private String mode;
 
-    Language(String description, String extension) {
+    Language(String description, String extension, String mode) {
         this.description = description;
         this.extension = extension;
+        this.mode = mode;
     }
 
     public static Language from(String s) {
@@ -23,5 +25,9 @@ public enum Language {
 
     public String getExtension() {
         return extension;
+    }
+
+    public String getMode() {
+        return mode;
     }
 }
