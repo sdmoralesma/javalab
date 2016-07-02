f6089a15bc5e75e3f4cbc28f9435c8f1a6b6cdf7
Sergio <sdmoralesma@gmail.com>
2016-07-02 17:18:46 +0200

    fixed prodMod

--- javalab/src/main/webapp/app/boot.js
+++ javalab/src/main/webapp/app/boot.js
@@ -5,7 +5,8 @@ require("rxjs/Rx");
 var http_1 = require("@angular/http");
 var tag_service_1 = require("./tag.service");
 var javalab_service_1 = require("./javalab.service");
-enableProdMode();
+var core_1 = require("@angular/core");
+core_1.enableProdMode();
 platform_browser_dynamic_1.bootstrap(app_component_1.AppComponent, [
     http_1.HTTP_PROVIDERS,
     tag_service_1.TagService,
