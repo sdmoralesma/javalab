201f9287b11f49fe2c9987e0a214a83ad64fa15c
Sergio <sdmoralesma@gmail.com>
2016-07-06 23:17:43 +0200

    added integration test

---
+++ javalab-st/src/test/java/WorkspaceProcessorResourceIT.java
@@ -0,0 +1,21 @@
+import com.airhacks.rulz.jaxrsclient.JAXRSClientProvider;
+import org.junit.Test;
+
+import javax.json.JsonObject;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class WorkspaceProcessorResourceIT {
+
+    public JAXRSClientProvider provider = JAXRSClientProvider.buildWithURI("http://localhost:48080/rest/process");
+
+    @Test
+    public void shouldGetJavaWorkspace() {
+        Response response = provider.addPath("/init/java").request(MediaType.APPLICATION_JSON).get();
+        assertThat(response.getStatus()).isEqualTo(200);
+        JsonObject result = response.readEntity(JsonObject.class);
+        assertThat(result.getString("terminal")).isNotNull().contains("Welcome to Javalab");
+    }
+}
