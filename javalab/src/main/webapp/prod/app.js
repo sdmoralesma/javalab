906d6b70fc0f2dc82b2bc29d5e9897f69e77ee1b
Sergio <sdmoralesma@gmail.com>
2016-08-28 17:00:13 +0200

    +prod-mode

--- javalab/src/main/webapp/prod/app.js
+++ javalab/src/main/webapp/prod/app.js
@@ -6,8 +6,7 @@ webpackJsonp([0],{
 	"use strict";
 	var platform_browser_dynamic_1 = __webpack_require__(1);
 	var main_module_1 = __webpack_require__(337);
-	var core_1 = __webpack_require__(11);
-	core_1.enableProdMode();
+	enableProdMode();
 	platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(main_module_1.MainModule);
 	//# sourceMappingURL=main.js.map
 
