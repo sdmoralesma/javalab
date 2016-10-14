eba6076a4e0dc7292785787cb56f8695916cdb00
Sergio Morales <sergio@backbase.com>
2016-10-14 12:16:36 +0200

    upgraded dependencies

--- javalab/src/test/java/com/smorales/javalab/business/files/FileManagerTest.java
+++ javalab/src/test/java/com/smorales/javalab/business/files/FileManagerTest.java
@@ -1,37 +1,20 @@
 package com.smorales.javalab.business.files;
 
-import com.smorales.javalab.business.processor.boundary.LabPaths;
-import com.smorales.javalab.business.NotRunnableCodeException;
 import com.smorales.javalab.business.processor.boundary.SimpleNode;
 import com.smorales.javalab.business.processor.boundary.rest.request.TreeNode;
 import com.smorales.javalab.business.processor.creators.FileManagerCreators;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Matchers;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Logger;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.*;
-import static org.powermock.api.mockito.PowerMockito.mockStatic;
-import static org.powermock.api.mockito.PowerMockito.verifyStatic;
+import static org.mockito.Mockito.mock;
 
-@RunWith(PowerMockRunner.class)
-@PrepareForTest(FileManager.class)
 public class FileManagerTest {
 
     private static final String REGEX_UUID = "[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}";
@@ -41,34 +24,6 @@ public class FileManagerTest {
     public void setUp() {
         sut = new FileManager();
         sut.tracer = mock(Logger.class);
-    }
-
-    @Test
-    public void shouldCreateTempDir() throws Exception {
-        mockStatic(Paths.class, Files.class);
-
-        when(Paths.get(anyString())).thenReturn(mock(Path.class));
-        when(Files.createDirectory(any(Path.class))).thenReturn(mock(Path.class));
-        doNothing().when(sut.tracer).info(Matchers::anyString);
-
-        Path result = sut.createTempDir();
-
-        ArgumentCaptor<String> argument = ArgumentCaptor.forClass(String.class);
-        verifyStatic();
-        Paths.get(argument.capture());
-
-        assertThat(result).isNotNull();
-        assertThat(argument.getValue()).matches(LabPaths.HOME.asString() + REGEX_UUID + File.separator);
-    }
-
-    @Test
-    public void shouldThrowExceptionWhenCreateTempDirThrowsIOException() throws Exception {
-        mockStatic(Paths.class, Files.class);
-
-        when(Paths.get(anyString())).thenReturn(mock(Path.class));
-        when(Files.createDirectory(any(Path.class))).thenThrow(new IOException());
-
-        assertThatThrownBy(sut::createTempDir).isInstanceOf(NotRunnableCodeException.class);
     }
 
     @Test
